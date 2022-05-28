1b2b5ec1ec30a6c1ab64bdb5f27876c0928a4427
sdott3 <sdotthomas347@gmail.com>
2022-05-28 17:44:33 -0400

    variables set in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,31 @@
-var startBtn = document.querySelector("startBtn");
+var timer = 60;
+var score = 0;
 
+var startButton = document.getElementById("startBtn");
+var quesContainer = document.getElementById("questionContainer");
+var quesElement = document.getElementById("questions");
+var answerButton = document.getElementById("answers");
+var countdown = document.getElementById("timer");
+
+
+// Start Game function; click the start button
+function startQuiz() {
+    console.log("startBtn");
+
+}
+
+// timer starts and I am presented with a question
+
+
+// Select a answer
+function selectAnswer() {
+
+}
+
+
+// Alert if question is right or wrong
+// time is subtracted from the clock
+
+
+// Add event listener to start button
+startButton.addEventListener("click", startQuiz);
